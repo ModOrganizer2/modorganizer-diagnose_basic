0cd3f8232e5ec2b676bd95f4344156b63ae0c467
Tannin <sherb@gmx.net>
2016-06-04 18:11:39 +0200

    translation

---
+++ src/diagnose_basic_en.ts
@@ -0,0 +1,109 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="en_US">
+<context>
+    <name>DiagnoseBasic</name>
+    <message>
+        <location filename="diagnosebasic.cpp" line="95"/>
+        <source>Checks for problems unrelated to other plugins</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="diagnosebasic.cpp" line="111"/>
+        <source>Warn when an error occured last time an application was run</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="diagnosebasic.cpp" line="112"/>
+        <source>Warn when there are files in the overwrite directory</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="diagnosebasic.cpp" line="113"/>
+        <source>Warn when the font configuration refers to files that aren&apos;t installed</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="diagnosebasic.cpp" line="114"/>
+        <source>Warn when mods are installed that conflict with MO functionality</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="diagnosebasic.cpp" line="115"/>
+        <source>Warn when there are esps with missing masters</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="diagnosebasic.cpp" line="116"/>
+        <source>Ignore empty directories when checking overwrite directory</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="diagnosebasic.cpp" line="117"/>
+        <source>Ignore .log files and empty directories when checking overwrite directory</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="diagnosebasic.cpp" line="338"/>
+        <source>There was an error reported recently</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="diagnosebasic.cpp" line="340"/>
+        <source>There are files in your overwrite mod</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="diagnosebasic.cpp" line="342"/>
+        <source>Your font configuration may be broken</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="diagnosebasic.cpp" line="344"/>
+        <source>Nitpick installed</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="diagnosebasic.cpp" line="346"/>
+        <source>Ini Tweaks overwritten</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="diagnosebasic.cpp" line="348"/>
+        <source>Missing Masters</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="diagnosebasic.cpp" line="350"/>
+        <location filename="diagnosebasic.cpp" line="389"/>
+        <location filename="diagnosebasic.cpp" line="405"/>
+        <source>invalid problem key %1</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="diagnosebasic.cpp" line="360"/>
+        <source>Files in the &lt;font color=&quot;red&quot;&gt;&lt;i&gt;Overwrite&lt;/i&gt;&lt;/font&gt; mod are are usually files created by an external tool (i.e. Wrye Bash, Automatic Variants, ...).&lt;br&gt;It is advisable you empty the Overwrite directory by moving those files to an existing mod. You can do this by double-clicking the &lt;font color=&quot;red&quot;&gt;&lt;i&gt;Overwrite&lt;/i&gt;&lt;/font&gt; mod and use drag&amp;drop to move the files to a mod.&lt;br&gt;Alternatively, right-click on &lt;font color=&quot;red&quot;&gt;&lt;i&gt;Overwrite&lt;/i&gt;&lt;/font&gt; and create a new regular mod from the files there.&lt;br&gt;&lt;br&gt;Why is this necessary? Generated files may depend on the other mods active in a profile and may thus be incompatible with a different profile (i.e. bashed patches from Wrye Bash). On the other hand the file may be necessary in all profiles (i.e. dlc esms after cleaning with TESVEdit)&lt;br&gt;This can NOT be automated you HAVE to read up on the tools you use and make an educated decision.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="diagnosebasic.cpp" line="368"/>
+        <source>Your current configuration seems to reference a font that is not installed. You may see only boxes instead of letters.&lt;br&gt;The font configuration is in Data\interface\fontconfig.txt. Most likely you have a broken installation of a font replacer mod.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="diagnosebasic.cpp" line="371"/>
+        <source>You have the nitpick skse plugin installed. This plugin is not needed with Mod Organizer because MO already offers the same functionality. Worse: The two solutions may conflict so it&apos;s strongly suggested you remove this plugin.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="diagnosebasic.cpp" line="375"/>
+        <source>Settings provided in ini tweaks have been overwritten in-game or in an applications.&lt;br&gt;These overwrites are stored in a separate file (&lt;i&gt;profile_tweaks.ini&lt;/i&gt; within the profile directory)&lt;br&gt;to keep ini-tweaks in their original state but you should really get rid of this file as there is&lt;br&gt;no tool support in MO to work on it. &lt;br&gt;Advice: Copy settings you want to keep to an appropriate ini tweak, then delete &lt;i&gt;profile_tweaks.ini&lt;/i&gt;.&lt;br&gt;Hitting the &lt;i&gt;Fix&lt;/i&gt; button will delete that file</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="diagnosebasic.cpp" line="384"/>
+        <source>The masters for some plugins (esp/esm) are not enabled.&lt;br&gt;The game will crash unless you install and enable the following plugins: </source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+</TS>
