97f88f15e3a9b6999617229014775e7873dbdc08
Mikaël Capelle <capelle.mikael@gmail.com>
2025-05-29 10:59:43 +0200

    Move to VCPKG. (#17)

--- src/diagnose_basic_en.ts
+++ src/diagnose_basic_en.ts
@@ -4,173 +4,173 @@
 <context>
     <name>DiagnoseBasic</name>
     <message>
-        <location filename="diagnosebasic.cpp" line="101"/>
+        <location filename="diagnosebasic.cpp" line="92"/>
         <source>Checks for problems unrelated to other plugins</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="diagnosebasic.cpp" line="117"/>
+        <location filename="diagnosebasic.cpp" line="108"/>
         <source>Warn when an error occurred last time an application was run</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="diagnosebasic.cpp" line="118"/>
+        <location filename="diagnosebasic.cpp" line="109"/>
         <source>Warn when there are files in the overwrite directory</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="diagnosebasic.cpp" line="119"/>
+        <location filename="diagnosebasic.cpp" line="110"/>
         <source>Warn when the font configuration refers to files that aren&apos;t installed</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="diagnosebasic.cpp" line="120"/>
+        <location filename="diagnosebasic.cpp" line="111"/>
         <source>Warn when mods are installed that conflict with MO functionality</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="diagnosebasic.cpp" line="121"/>
+        <location filename="diagnosebasic.cpp" line="112"/>
         <source>Warn when there are esps with missing masters</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="diagnosebasic.cpp" line="122"/>
+        <location filename="diagnosebasic.cpp" line="113"/>
         <source>Warn when an installed mod came from an alternative game source</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="diagnosebasic.cpp" line="123"/>
+        <location filename="diagnosebasic.cpp" line="114"/>
         <source>Warn when files have unwanted attributes</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="diagnosebasic.cpp" line="124"/>
+        <location filename="diagnosebasic.cpp" line="115"/>
         <source>Ignore empty directories when checking overwrite directory</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="diagnosebasic.cpp" line="125"/>
+        <location filename="diagnosebasic.cpp" line="116"/>
         <source>Ignore .log files and empty directories when checking overwrite directory</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="diagnosebasic.cpp" line="507"/>
+        <location filename="diagnosebasic.cpp" line="569"/>
         <source>File attribute checker
 Searching for problems...</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="diagnosebasic.cpp" line="537"/>
+        <location filename="diagnosebasic.cpp" line="602"/>
         <source>Invalid file attributes found</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="diagnosebasic.cpp" line="538"/>
+        <location filename="diagnosebasic.cpp" line="603"/>
         <source>One or more of your files has attributes that may prevent the game from reading the files. This can result in missing plugins, missing textures, and other such problems.
 
 Fix the file attributes?</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="diagnosebasic.cpp" line="551"/>
+        <location filename="diagnosebasic.cpp" line="618"/>
         <source>File attribute checker
 Fixing file attributes...</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="diagnosebasic.cpp" line="564"/>
+        <location filename="diagnosebasic.cpp" line="633"/>
         <source>Unable to set file attributes</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="diagnosebasic.cpp" line="565"/>
+        <location filename="diagnosebasic.cpp" line="634"/>
         <source>Mod Organizer was unable to fix the file attributes of at least one file.
 
 Continue launching %1?</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="diagnosebasic.cpp" line="608"/>
+        <location filename="diagnosebasic.cpp" line="687"/>
         <source>There was an error reported recently</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="diagnosebasic.cpp" line="610"/>
+        <location filename="diagnosebasic.cpp" line="689"/>
         <source>There are files in your Overwrite mod directory</source>
         <oldsource>There are files in your overwrite mod</oldsource>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="diagnosebasic.cpp" line="612"/>
+        <location filename="diagnosebasic.cpp" line="691"/>
         <source>Your font configuration may be broken</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="diagnosebasic.cpp" line="614"/>
+        <location filename="diagnosebasic.cpp" line="693"/>
         <source>Nitpick installed</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="diagnosebasic.cpp" line="616"/>
+        <location filename="diagnosebasic.cpp" line="695"/>
         <source>INI Tweaks overwritten</source>
         <oldsource>Ini Tweaks overwritten</oldsource>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="diagnosebasic.cpp" line="618"/>
+        <location filename="diagnosebasic.cpp" line="697"/>
         <source>Missing Masters</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="diagnosebasic.cpp" line="620"/>
+        <location filename="diagnosebasic.cpp" line="699"/>
         <source>At least one unverified mod is using an alternative game source</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="diagnosebasic.cpp" line="622"/>
-        <location filename="diagnosebasic.cpp" line="680"/>
-        <location filename="diagnosebasic.cpp" line="696"/>
+        <location filename="diagnosebasic.cpp" line="701"/>
+        <location filename="diagnosebasic.cpp" line="763"/>
+        <location filename="diagnosebasic.cpp" line="782"/>
         <source>invalid problem key %1</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="diagnosebasic.cpp" line="632"/>
+        <location filename="diagnosebasic.cpp" line="712"/>
         <source>There are currently files in your &lt;span style=&quot;color: red;&quot;&gt;&lt;i&gt;Overwrite&lt;/i&gt;&lt;/span&gt; directory. These files are typically newly created files, usually generated by an external mod tool (i.e. Wrye Bash, xEdit, FNIS, ...). Creation Club ESL files will also end up here when downloaded. Any files in &lt;span style=&quot;font-weight: bold;&quot;&gt;Overwrite&lt;/span&gt; will take top priority when loading your mod files and will always overwrite any other mod in your profile.&lt;br&gt;&lt;br&gt;It is recommended that you review the files in &lt;span style=&quot;font-weight: bold;&quot;&gt;Overwrite&lt;/span&gt; and move any relevant files to a new or existing mod. You can do this by double-clicking the &lt;span style=&quot;font-weight: bold;&quot;&gt;Overwrite&lt;/span&gt; mod and dragging files from the Overwrite window to a mod entry in the main mod list. It is also possible to move all current &lt;span style=&quot;font-weight: bold;&quot;&gt;Overwrite&lt;/span&gt; files to a new mod by right-clicking on the Overwrite mod.&lt;br&gt;&lt;br&gt;Not all files in &lt;span style=&quot;font-weight: bold;&quot;&gt;Overwrite&lt;/span&gt; need to be removed, but there are several reasons to do so. Some generated files will be directly related to the active mods in your profile and will be incompatible with different mod setups. Since &lt;span style=&quot;font-weight: bold;&quot;&gt;Overwrite&lt;/span&gt; is always active, this could cause conflicts between profiles. Additionally, moving relevant game files into a normal mod will give you greater control over those files. Some files can live safely in &lt;span style=&quot;font-weight: bold;&quot;&gt;Overwrite&lt;/span&gt;, such as basic logs and cache files. It is up to you to understand how best to manage these files.&lt;br&gt;&lt;br&gt;If you do not wish to see this warning and understand how to handle your &lt;span style=&quot;font-weight: bold;&quot;&gt;Overwrite&lt;/span&gt; directory, you can open the Mod Organizer settings and disable this warning under the &quot;Diagnose Basic&quot; plugin configuration.</source>
         <oldsource>There are currently files in your &lt;font color=&quot;red&quot;&gt;&lt;i&gt;Overwrite&lt;/i&gt;&lt;/font&gt; directory. These files are typically newly created files, usually generated by an external mod tool (i.e. Wrye Bash, xEdit, FNIS, ...). Creation Club ESL files will also end up here when downloaded. Any files in &lt;font color=&quot;red&quot;&gt;&lt;i&gt;Overwrite&lt;/i&gt;&lt;/font&gt; will take top priority when loading your mod files and will always overwrite any other mod in your profile.&lt;br&gt;&lt;br&gt;It is recommended that you review the files in &lt;font color=&quot;red&quot;&gt;&lt;i&gt;Overwrite&lt;/i&gt;&lt;/font&gt; and move any relevant files to a new or existing mod. You can do this by double-clicking the &lt;font color=&quot;red&quot;&gt;&lt;i&gt;Overwrite&lt;/i&gt;&lt;/font&gt; mod and dragging files from the &lt;font color=&quot;red&quot;&gt;&lt;i&gt;Overwrite&lt;/i&gt;&lt;/font&gt; window to a mod entry in the main mod list. It is also possible to move all current &lt;font color=&quot;red&quot;&gt;&lt;i&gt;Overwrite&lt;/i&gt;&lt;/font&gt; files to a new mod by right-clicking on the &lt;font color=&quot;red&quot;&gt;&lt;i&gt;Overwrite&lt;/i&gt;&lt;/font&gt; mod.&lt;br&gt;&lt;br&gt;Not all files in &lt;font color=&quot;red&quot;&gt;&lt;i&gt;Overwrite&lt;/i&gt;&lt;/font&gt; need to be removed, but there are several reasons to do so. Some generated files will be directly related to the active mods in your profile and will be incompatible with different mod setups. Since &lt;font color=&quot;red&quot;&gt;&lt;i&gt;Overwrite&lt;/i&gt;&lt;/font&gt; is always active, this could cause conflicts between profiles. Additionally, moving relevant game files into a normal mod will give you greater control over those files. Some files can live safely in &lt;font color=&quot;red&quot;&gt;&lt;i&gt;Overwrite&lt;/i&gt;&lt;/font&gt;, such as basic logs and cache files. It is up to you to understand how best to manage these files.&lt;br&gt;&lt;br&gt;If you do not wish to see this warning and understand how to handle your &lt;font color=&quot;red&quot;&gt;&lt;i&gt;Overwrite&lt;/i&gt;&lt;/font&gt; directory, you can open the Mod Organizer settings and disable this warning under the &quot;Diagnose Basic&quot; plugin configuration.</oldsource>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="diagnosebasic.cpp" line="640"/>
+        <location filename="diagnosebasic.cpp" line="720"/>
         <source>Your current configuration seems to reference a font that is not installed. You may see only boxes instead of letters.&lt;br&gt;The font configuration is in Data\interface\fontconfig.txt. Most likely you have a broken installation of a font replacer mod.</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="diagnosebasic.cpp" line="643"/>
+        <location filename="diagnosebasic.cpp" line="723"/>
         <source>You have the nitpick skse plugin installed. This plugin is not needed with Mod Organizer because MO already offers the same functionality. Worse: The two solutions may conflict so it&apos;s strongly suggested you remove this plugin.</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="diagnosebasic.cpp" line="647"/>
+        <location filename="diagnosebasic.cpp" line="728"/>
         <source>Settings provided in ini tweaks have been overwritten in-game or in an applications.&lt;br&gt;These overwrites are stored in a separate file (&lt;i&gt;profile_tweaks.ini&lt;/i&gt; within the profile directory)&lt;br&gt;to keep ini-tweaks in their original state but you should really get rid of this file as there is&lt;br&gt;no tool support in MO to work on it. &lt;br&gt;Advice: Copy settings you want to keep to an appropriate ini tweak, then delete &lt;i&gt;profile_tweaks.ini&lt;/i&gt;.&lt;br&gt;Hitting the &lt;i&gt;Fix&lt;/i&gt; button will delete that file</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="diagnosebasic.cpp" line="664"/>
+        <location filename="diagnosebasic.cpp" line="747"/>
         <source>The masters for some plugins (esp/esl/esm) are not enabled.&lt;br&gt;The game will crash unless you install and enable the following plugins: </source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="diagnosebasic.cpp" line="667"/>
+        <location filename="diagnosebasic.cpp" line="749"/>
         <source>Master</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="diagnosebasic.cpp" line="668"/>
+        <location filename="diagnosebasic.cpp" line="749"/>
         <source>Required By</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="diagnosebasic.cpp" line="672"/>
+        <location filename="diagnosebasic.cpp" line="754"/>
         <source>You have at least one active mod installed from an alternative game source.&lt;br&gt;This means that the mod was downloaded from a game source which does not match&lt;br&gt;the expected primary game.&lt;br&gt;&lt;br&gt;Depending on the type of mod, this may require converting various files to run correctly.&lt;br&gt;&lt;br&gt;Advice: Once you have verified the mod is working correctly, you can use the context menu&lt;br&gt;and select &quot;Mark as converted/working&quot; to remove the flag and warning.</source>
         <translation type="unfinished"></translation>
     </message>
